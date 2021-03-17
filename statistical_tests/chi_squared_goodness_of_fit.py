08579ccb9b22dd1019a5bfc768c7438249375adc
felsiq <felipe.siqueira@usp.br>
2021-03-17 18:33:54 -0300

    Updated documentation

--- statistical_tests/chi_squared_goodness_of_fit.py
+++ statistical_tests/chi_squared_goodness_of_fit.py
@@ -2,9 +2,40 @@ import scipy.stats
 import numpy as np
 
 
+def g_test(data_counts, expected_counts, ddof: int = 1):
+    """Chi-squared test for goodness of fit using likelihood ratio statistic."""
+    return chi_squared_goodness_of_fit(
+        data_counts, expected_counts, ddof, statistic="llikelihood_ratio"
+    )
+
+
 def chi_squared_goodness_of_fit(
     data_counts, expected_counts, ddof: int = 1, statistic: str = "likelihood_ratio"
 ):
+    """Chi-squared test for goodness of fit.
+
+    Checks whether a given count matches the expected counts from some
+    known distribution.
+
+    Assumptions:
+        None.
+
+    Test statistic:
+        If statistic=`likelihood_ratio`, G = 2 * dot(O, log(O / E))
+        If statistic=`pearson_chi_square`, X^{2} = sum((O - E)^2 / E)
+    Where:
+        `O` is the data_counts;
+        `E` is the expected_counts;
+        log is the natural log.
+
+    Null distribution: G ~ Chi^2(df) and X^{2} ~ Chi^2(df), where Chi^2
+        is the Chi^2 distribution. Also, G is approximately X^{2} under
+        the null hypothesis, and usually df = data_bins - 1, but may vary
+        depending on the problem.
+
+    H0: the data distribution matches the expected distribution.
+    HA: the data distribution follows another latent distribution.
+    """
     assert statistic in {"likelihood_ratio", "pearson_chi_square"}
 
     if statistic == "likelihood_ratio":
