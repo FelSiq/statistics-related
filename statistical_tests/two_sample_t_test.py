2e2bb2a77c58db7e0e3a50264c12c6168000f857
felsiq <felipe.siqueira@usp.br>
2021-03-17 14:39:21 -0300

    Add welch's test for two-sample t-test with unequal vars

--- statistical_tests/two_sample_t_test.py
+++ statistical_tests/two_sample_t_test.py
@@ -2,6 +2,39 @@ import scipy.stats
 import numpy as np
 
 
+def welch_t_test(samples_x, samples_y, diff: float = 0.0, tail: str = "both"):
+    assert tail in {"both", "left", "right"}
+
+    mean_x = np.mean(samples_x)
+    mean_y = np.mean(samples_y)
+
+    n_x = len(samples_x)
+    n_y = len(samples_y)
+
+    var_x_norm = np.var(samples_x, ddof=1) / n_x
+    var_y_norm = np.var(samples_y, ddof=1) / n_y
+
+    pooled_var = var_x_norm + var_y_norm
+
+    test_statistic = (mean_x - mean_y - diff) / np.sqrt(pooled_var)
+
+    df = np.square(pooled_var) / (
+        np.square(var_x_norm) / (n_x - 1) + np.square(var_y_norm) / (n_y - 1)
+    )
+    null_dist = scipy.stats.t(df)
+
+    if tail == "both":
+        p_value = 2.0 * null_dist.cdf(-abs(test_statistic))
+
+    elif tail == "left":
+        p_value = null_dist.cdf(test_statistic)
+
+    else:
+        p_value = null_dist.sf(test_statistic)
+
+    return test_statistic, p_value
+
+
 def two_sample_t_test_equal_var(
     samples_x, samples_y, diff: float = 0.0, tail: str = "both"
 ):
@@ -67,14 +100,21 @@ def _test():
             sample_x = mean_x + sample_std * np.random.randn(200)
             sample_y = mean_y + sample_std * np.random.randn(100)
             res = two_sample_t_test_equal_var(sample_x, sample_y, diff=diff, tail=tail)
-            print(tail, res)
+            scipy_res = scipy.stats.ttest_ind(
+                sample_x, sample_y, equal_var=True, alternative=tail_scipy
+            )
+
+            print(tail, res, scipy_res)
             if np.isclose(0, diff):
-                assert np.allclose(
-                    res,
-                    scipy.stats.ttest_ind(
-                        sample_x, sample_y, equal_var=True, alternative=tail_scipy
-                    ),
-                )
+                assert np.allclose(res, scipy_res)
+
+            res_welch = welch_t_test(sample_x, sample_y, diff=diff, tail=tail)
+            scipy_welch_res = scipy.stats.ttest_ind(
+                sample_x, sample_y, equal_var=False, alternative=tail_scipy
+            )
+            print(tail, res_welch, scipy_welch_res)
+            if np.isclose(0, diff):
+                assert np.allclose(res_welch, scipy_welch_res)
 
 
 if __name__ == "__main__":
